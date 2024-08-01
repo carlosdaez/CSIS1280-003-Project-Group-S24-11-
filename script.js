25ea014619e97f08d25deef99f3c1f5ea7ca6ef0
carlosdaez <carlos.daez@gmail.com>
2024-07-31 20:37:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,14 @@ function showAlert() {
     var popup = document.getElementById("popup");
     var closeBtn = document.querySelector(".close");
 
-    // Show the popup after a delay (e.g., 3 seconds)
     setTimeout(function() {
         popup.style.display = "block";
-    }, 5000);
+    }, 3000);
 
-    // Close the popup when the close button is clicked
     closeBtn.onclick = function() {
         popup.style.display = "none";
     }
 
-    // Close the popup when clicking outside of the popup content
     window.onclick = function(event) {
         if (event.target == popup) {
             popup.style.display = "none";
@@ -26,5 +23,4 @@ function showAlert() {
 
 const productImagesContainer = document.querySelector('.product-images');
 
-// Duplicate the product images to create a seamless loop
 productImagesContainer.innerHTML += productImagesContainer.innerHTML;
